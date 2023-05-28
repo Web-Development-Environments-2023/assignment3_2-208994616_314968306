9159663a4b1e722d788c67c47e0356ce6510f8b2
Saar <saarb97@gmail.com>
2023-05-28 19:29:47 +0300

    initial

---
+++ sql scripts/sql_create_tables.sql
@@ -0,0 +1,26 @@
+-- Active: 1685280459385@@127.0.0.1@3306@hw
+CREATE TABLE Users (
+  username VARCHAR(8) PRIMARY KEY NOT NULL,
+  firstname VARCHAR(255) NOT NULL,
+  lastname VARCHAR(255) NOT NULL,
+  country VARCHAR(255) NOT NULL,
+  password VARCHAR(255) NOT NULL,
+  email VARCHAR(255) NOT NULL
+);
+
+-- CREATE TABLE Recipes (
+--   id INT PRIMARY KEY AUTO_INCREMENT,
+--   RecipeImg VARCHAR(255) NOT NULL,
+--   RecipeID VARCHAR(255) NOT NULL,
+--   RecipeName VARCHAR(255) NOT NULL,
+--   CookingTime INT NOT NULL,
+--   Likes INT NOT NULL,
+--   GlutenFree BOOLEAN NOT NULL,
+--   isVegan BOOLEAN NOT NULL,
+--   isVegetarian BOOLEAN NOT NULL,
+--   isClicked BOOLEAN NOT NULL,
+--   isFavorite BOOLEAN NOT NULL,
+--   ingredients TEXT NOT NULL,
+--   instructions TEXT NOT NULL,
+--   servings INT NOT NULL
+-- );
