6b1ca4195dc839adfa9fd0f81376387dd71ac903
Saar <saarb97@gmail.com>
2023-05-29 15:29:32 +0300

    main.js fixes

--- routes/user.js
+++ routes/user.js
@@ -8,14 +8,15 @@ const recipe_utils = require("./utils/recipes_utils");
  * Authenticate all incoming requests by middleware
  */
 router.use(async function (req, res, next) {
-  if (req.session && req.session.user_id) {
-    DButils.execQuery("SELECT user_id FROM users").then((users) => {
-      if (users.find((x) => x.user_id === req.session.user_id)) {
-        req.user_id = req.session.user_id;
+  if (req.session && req.session.username) {
+    DButils.execQuery("SELECT username FROM users").then((users) => {
+      if (users.find((x) => x.username === req.session.username)) {
+        req.username = req.session.username;
         next();
       }
     }).catch(err => next(err));
   } else {
+    console.log(req.session.username)
     res.sendStatus(401);
   }
 });
@@ -24,11 +25,11 @@ router.use(async function (req, res, next) {
 /**
  * This path gets body with recipeId and save this recipe in the favorites list of the logged-in user
  */
-router.post('/favorites', async (req,res,next) => {
+router.post('/AddToFavorites', async (req,res,next) => {
   try{
-    const user_id = req.session.user_id;
-    const recipe_id = req.body.recipeId;
-    await user_utils.markAsFavorite(user_id,recipe_id);
+    const username = req.session.username;
+    const recipeID = req.params.recipeId;
+    await user_utils.markAsFavorite(username,recipeID);
     res.status(200).send("The Recipe successfully saved as favorite");
     } catch(error){
     next(error);
@@ -38,13 +39,13 @@ router.post('/favorites', async (req,res,next) => {
 /**
  * This path returns the favorites recipes that were saved by the logged-in user
  */
-router.get('/favorites', async (req,res,next) => {
+router.get('/getFavorites', async (req,res,next) => {
   try{
-    const user_id = req.session.user_id;
+    const username = req.session.username;
     let favorite_recipes = {};
-    const recipes_id = await user_utils.getFavoriteRecipes(user_id);
+    const recipeID = await user_utils.getFavoriteRecipes(username);
     let recipes_id_array = [];
-    recipes_id.map((element) => recipes_id_array.push(element.recipe_id)); //extracting the recipe ids into array
+    recipeID.map((element) => recipes_id_array.push(element.recipeID)); //extracting the recipe ids into array
     const results = await recipe_utils.getRecipesPreview(recipes_id_array);
     res.status(200).send(results);
   } catch(error){
