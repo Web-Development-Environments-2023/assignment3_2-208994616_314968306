78f667e66a7089c80c6432317168c1cff755e877
Saar <saarb97@gmail.com>
2023-06-01 17:08:20 +0300

    added getCreatedRecipes function

--- routes/user.js
+++ routes/user.js
@@ -39,10 +39,8 @@ router.post('/addToFavorites', async (req,res,next) => {
  * This path returns the favorites recipes that were saved by the logged-in user
  */
 router.get('/getFavorites', async (req,res,next) => {
-  console.log("saar")
   try{
     const username = req.session.username;
-    // let favorite_recipes = {};
     const recipe_ids_dict = await user_utils.getFavoriteRecipes(username);
     let recipe_ids_array = [];
     recipe_ids_dict.map((element) => recipe_ids_array.push(element.recipeID)); //extracting the recipe ids into array
@@ -52,6 +50,17 @@ router.get('/getFavorites', async (req,res,next) => {
     next(error); 
   }
 });
+
+router.get('/getCreatedRecipes', async (req,res,next) => {
+  try{
+    const username = req.session.username;
+    const recipes = await recipe_utils.getCreatedRecipes(username);
+    res.status(200).send(recipes);
+  } catch(error){
+    next(error); 
+  }
+});
+
 
 /**
  * This path gets body with recipeId and save this recipe in the watched list of the logged-in user
@@ -74,13 +83,10 @@ router.get('/threeLastWatchedRecipes', async (req,res,next) => {
   try{
     const username = req.session.username;
     const recipe_ids_dict = await user_utils.getWatchedRecipes(username);
-    console.log("id = " + recipe_ids_dict[0]);
     let recipes_id_array = [];
     recipe_ids_dict.map((element) => recipes_id_array.push(element.recipeID)); //extracting the recipe ids into array
     recipes_id_array = recipes_id_array.slice(0, 3);
-    console.log("id = " + recipes_id_array[0]);
     const results = await recipe_utils.arrayOfIdToPreviewRecipes(recipes_id_array);
-    console.log("13")
     res.status(200).send(results);
   } catch(error){
     next(error); 
@@ -90,7 +96,7 @@ router.get('/threeLastWatchedRecipes', async (req,res,next) => {
 router.post("/addNewRecipe", async (req, res, next) => {
   try {
       const new_recipe = await recipe_utils.addNewRecipe(req.session.username,req.body);
-      res.status(200).send(new_recipe); // TODO FIX ERROR HERE - Probably wrong send type
+      res.status(200).send("The Recipe was successfully added");; // can also send new_recipe if needed - preview
   } catch (error) {
       next(error);
   }
--- routes/utils/recipes_utils.js
+++ routes/utils/recipes_utils.js
@@ -50,11 +50,10 @@ function filterRecipes(recipe) {
 }
 
 async function addNewRecipe(username, recipe) {
-    // REMOVING LIKES MAYBE??
     try {
         const query = `INSERT INTO UserRecipes (username, RecipeImg, RecipeName, CookingTime, Likes, GlutenFree, isVegan, isVegetarian, ingredients, instructions, servings)
                    VALUES ('${username}', '${recipe.RecipeImg}', '${recipe.RecipeName}', ${recipe.CookingTime}
-                   , 0, ${recipe.GlutenFree}, ${recipe.isVegan}, ${recipe.isVegetarian}
+                   , ${recipe.GlutenFree}, ${recipe.isVegan}, ${recipe.isVegetarian}
                    , '${recipe.ingredients}', '${recipe.instructions}', ${recipe.servings})`;
 
         const result = await DButils.execQuery(query);
@@ -69,12 +68,32 @@ async function addNewRecipe(username, recipe) {
             vegetarian: recipe.isVegetarian,
             glutenFree: recipe.GlutenFree
         };
-        return out;
+        return out; // Returns preview recipe but not utilized
         } catch (error) {
             console.error('Error adding new recipe:', error);
             throw error;
         }
-    }
+}
+async function getCreatedRecipes(username){
+    const fullrecipes_array = await DButils.execQuery(`select * from UserRecipes where username='${username}'`);
+    recipes_array = []
+    fullrecipes_array.forEach(element => {
+        let { recipeID, RecipeName, CookingTime, RecipeImg
+            , isVegan, isVegetarian, GlutenFree } = element;
+        preview_recipe = {
+                id: recipeID,
+                title: RecipeName,
+                readyInMinutes: CookingTime,
+                image: RecipeImg,
+                popularity: 0,
+                vegan: convertIntToBoolean(isVegan),
+                vegetarian: convertIntToBoolean(isVegetarian),
+                glutenFree: convertIntToBoolean(GlutenFree) 
+        }
+        recipes_array.push(preview_recipe);
+    });  
+    return recipes_array
+}
 
 /////////////////////////////////////////// Helping functions
 function fullToPreviewRecipe(fullRecipe) {
@@ -100,9 +119,19 @@ async function arrayOfIdToPreviewRecipes(recipes_id_array) {
     return res;
 }
 
+function convertIntToBoolean (num) {
+    if (num === 1) 
+        return true;
+    else if (num === 0) 
+        return false;
+    else
+        throw new Error("Invalid input. Expected 0 or 1.");
+}
+
 
 /////////////////////////////////////////// Exports
 exports.getRecipeDetails = getRecipePreview;
 exports.getThreeRandomRecipes = getThreeRandomRecipes;
 exports.arrayOfIdToPreviewRecipes = arrayOfIdToPreviewRecipes;
 exports.addNewRecipe = addNewRecipe;
+exports.getCreatedRecipes = getCreatedRecipes;
